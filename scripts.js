2c8e569ce016fadbd2cff1c22cddcae8483c821a
AlePaff <alejandropaff@gmail.com>
2023-02-11 02:33:55 -0300

    border y estilos +comentarios eliminados

--- scripts.js
+++ scripts.js
@@ -84,45 +84,8 @@ function load_section(id) {
 }
 
 
-$(document).ready(function () {     //cuando el documento este listo es decir cuando se cargue la pagina
-
-    // añadir como hijo del div container a la clase "donut"
-    // document.querySelector(".container").innerHTML = load_imagemap("donut");
+$(document).ready(function () {
 
     load_section("donut");
-
-
-    // $('img').mapster({           //recordar: img[usemap] targetea a los que tengan img y el atributo usemap
-    //     // fillColor: 'ff0000',        //color de relleno 
-    //     fill: true,                 //si se rellena el area del color fillColor
-    //     stroke: true,
-    //     singleSelect: true,      //si se puede clickear mas de un area
-    //     mapKey: 'color',       //la clave que se usa para identificar los areas (ver html)
-    //     strokeWidth: 2,
-    // 	staticState: true,          //para que un area esté siempre seleccionada
-
-
-    //     //hace que las areas con clave 'green' tenga un stroke de color especificado
-    //     areas: [{
-    //         key: 'blue',
-    //         strokeColor: '3498db',
-    //     },
-    //     {
-    //         key: 'green',
-    //         strokeColor: '2ecc71',
-    //         isSelectable: false         //no se puede clickear, siempre permanece seleccionado
-    //     },
-    //     {
-    //         key: 'yellow',
-    //         strokeColor: 'f1c40f',
-    //     }]
-    // });
-
-    // $('area.group1').mapster('select');
-
-    // .empty()    //elimina todos los hijos del elemento seleccionado
-
-
-
 
 });
